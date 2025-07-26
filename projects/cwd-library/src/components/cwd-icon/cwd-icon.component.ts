1336c563237020e73aefbb1a240d030db6dbe859
Daevel <luigi.avitabile5@gmail.com>
2025-07-27 01:24:51 +0200

    Added cwd-checkbox and cwd-checkbox-group to the library

--- projects/cwd-library/src/components/cwd-icon/cwd-icon.component.ts
+++ projects/cwd-library/src/components/cwd-icon/cwd-icon.component.ts
@@ -1,8 +1,8 @@
-import { NgIf } from '@angular/common';
 import { Component, Input, OnChanges } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgIf } from '@angular/common';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
-import * as solidIcons from '@fortawesome/free-solid-svg-icons';
+import * as fas from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'cwd-icon',
@@ -17,13 +17,12 @@ export class CwdIconComponent implements OnChanges {
 
   ngOnChanges(): void {
     if (this.iconName) {
-      // Costruisce il nome della proprietà, es: 'faCoffee'
       const iconKey = 'fa' + this.iconName.charAt(0).toUpperCase() + this.iconName.slice(1);
-      const iconCandidate = (solidIcons as any)[iconKey];
-      // Verifica che sia una vera icona FontAwesome
+      const iconCandidate = (fas as any)[iconKey];
       if (iconCandidate && iconCandidate.iconName) {
         this.icon = iconCandidate;
       } else {
+        console.warn(`Icona "${iconKey}" non trovata.`);
         this.icon = undefined;
       }
     }
--- projects/showcase/src/components/showcase/showcase.component.ts
+++ projects/showcase/src/components/showcase/showcase.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
-import { CwdCardComponent, CwdButtonComponent, CwdInputComponent, CwdIconComponent, CwdToastService, CwdFabComponent } from 'cwd-library';
+import { CwdCardComponent, CwdButtonComponent, CwdInputComponent, CwdIconComponent, CwdToastService, CwdFabComponent, CwdCheckboxComponent } from 'cwd-library';
 
 @Component({
   selector: 'app-showcase',
-  imports: [CwdCardComponent,CwdButtonComponent,CwdInputComponent,CwdIconComponent,CwdFabComponent],
+  imports: [CwdCardComponent, CwdButtonComponent, CwdInputComponent, CwdIconComponent, CwdCheckboxComponent],
   templateUrl: './showcase.component.html',
   styleUrl: './showcase.component.scss'
 })
 export class ShowcaseComponent {
 
   public testoDiProva: string = "";
+  isSelected: boolean = false;
 
   constructor(private toastService: CwdToastService) {
   }
